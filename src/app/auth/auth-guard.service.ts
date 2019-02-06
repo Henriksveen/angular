070360ca3709b451282ad4c931e58d06d88942fe
Simen Henriksveen <simen.henriksveen@gmail.com>
2019-02-06 23:19:59 +0100

    Using Angular Modules

--- src/app/auth/auth-guard.service.ts
+++ src/app/auth/auth-guard.service.ts
@@ -1,15 +1,19 @@
-import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot} from '@angular/router';
+import {ActivatedRouteSnapshot, CanActivate, CanLoad, Route, RouterStateSnapshot, UrlSegment} from '@angular/router';
 import {AuthService} from './auth.service';
 import {Injectable} from '@angular/core';
+import {RecipesRoutingModule} from '../recipes/recipes-routing.module';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class AuthGuard implements CanActivate {
+@Injectable()
+export class AuthGuard implements CanActivate, CanLoad {
   constructor(private authService: AuthService) {
   }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     return this.authService.isAuthenticated();
+  }
+
+  canLoad(route: Route, segments: UrlSegment[]) {
+    return true;
+    // return this.authService.isAuthenticated();
   }
 }
